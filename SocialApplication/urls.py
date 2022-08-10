34549e1af917c6871adff82343472fe52ddacbea
sreerag <sreeragk310@yahoo.com>
2022-08-10 10:56:26 +0530

    navbar extending out issue fixed

--- SocialApplication/urls.py
+++ SocialApplication/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blogapp.urls')),
+    path('',include('blogapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from blogapp import views
 urlpatterns=[
+    path("",views.SignUpView.as_view(),name="signup"),
     path("accounts/signup",views.SignUpView.as_view(),name="signup"),
     path("accounts/signin",views.LoginView.as_view(),name="signin"),
     path("accounts/signout", views.signout, name="signout"),
